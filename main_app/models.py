7366220d2a92054d98788622290d4dc6c0c08a56
annakreno <annakreno@gmail.com>
2023-02-13 17:30:21 -0600

    Andrew photo functionality add

--- main_app/models.py
+++ main_app/models.py
@@ -33,7 +33,12 @@ class Event(models.Model):
         return reverse('events_detail', kwargs={'event_id': self.id})
 
 
+class Photo(models.Model):
+    url = models.CharField(max_length=250)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Photo for event_id: {self.event_id} @{self.url}"
 
     
 
