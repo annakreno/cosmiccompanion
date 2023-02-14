65dc44ae2f98993dd003167fcbc7363fbff68ff4
andrewtheo9 <whitespy324@yahoo.com>
2023-02-13 20:32:38 -0800

    Photo functionality for events finished

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Event, CelestialObject
+from .models import Event, CelestialObject, Photo
 
 # Register your models here.
 admin.site.register(Event)
 admin.site.register(CelestialObject)
+admin.site.register(Photo)
